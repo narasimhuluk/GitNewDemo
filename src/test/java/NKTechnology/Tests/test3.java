b589b6ed8ec88f567d74004d0b9ddbe2647f2ead
narasimha <narasimha.k@intense.in>
2025-04-02 06:13:13 +0530

    updated test3

--- src/test/java/NKTechnology/Tests/test3.java
+++ src/test/java/NKTechnology/Tests/test3.java
@@ -1,9 +1,15 @@
 package NKTechnology.Tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("www.google.com");
 
 	}
 
